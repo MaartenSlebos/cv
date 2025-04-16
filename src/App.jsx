72978e79663ec4c7022b5e1ac92c05a99a7b6525
MaartenSlebos <maarten.slebos@hotmail.com>
2025-04-16 20:12:21 +0200

    add form states to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [activeForm, setActiveForm] = useState('general');
+
+
   return (
     <div className="app">
       <div className="app-container">
         <div id="left-button-row">
-          <button className="app-btn">General Information</button>
-          <button className="app-btn">Education Experience</button>
-          <button className="app-btn">Practical Experience</button>
+          <button className="app-btn" id="general-btn" onClick={() => setActiveForm('general')}>General Information</button>
+          <button className="app-btn" id="education-btn" onClick={() => setActiveForm('education')}>Education Experience</button>
+          <button className="app-btn" id="practical-btn" onClick={() => setActiveForm('practical')}>Practical Experience</button>
         </div>
         <div id="right-button-row"></div>
         <div id="left-card-row">
           <div id="left-card">
-
+            {activeForm === 'general' && <p>General Information Form</p>}
+            {activeForm === 'education' && <p>Education Experience Form</p>}
+            {activeForm === 'practical' && <p>Practical Experience Form</p>}
           </div>
         </div>
         <div id="right-card-row">
