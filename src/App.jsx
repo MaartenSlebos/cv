64df18d2f9c8c3b802ebe8f66811d426bf85e46c
MaartenSlebos <maarten.slebos@hotmail.com>
2025-04-16 18:32:45 +0200

    add buttons fix layout small screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,22 @@ function App() {
     <div className="app">
       <div className="app-title">CV Builder</div>
       <div className="app-container">
-        <div id="left-button-row"></div>
+        <div id="left-button-row">
+          <button className="app-btn">General Information</button>
+          <button className="app-btn">Education Experience</button>
+          <button className="app-btn">Practical Experience</button>
+        </div>
         <div id="right-button-row"></div>
-        <div id="left-card-row"></div>
-        <div id="right-card-row"></div>
+        <div id="left-card-row">
+          <div id="left-card">
+
+          </div>
+        </div>
+        <div id="right-card-row">
+          <div id="right-card">
+
+          </div>
+        </div>
       </div>
     </div>
   );
